eff4605d13a7ad2f1c882229a3a5bbdd3ebd8bad
mmmpa <mmmpa.mmmpa@gmail.com>
2015-02-12 12:25:25 -0800

    add binaryString

--- ts/chapter_1.ts
+++ ts/chapter_1.ts
@@ -33,7 +33,7 @@ function baseStringToValue(value: string, base: number): number {
 		if (value.length === 1) {
 			return convert(value) * this_base;
 		}
-		var tail: number = convert(value.charAt(value.length-1));
+		var tail: number = convert(value.charAt(value.length - 1));
 		var computed: number = tail * this_base;
 		return compute(value.slice(0, value.length - 1), this_base * base) + computed;
 	}
@@ -46,6 +46,62 @@ function baseStringToValue(value: string, base: number): number {
 	}
 
 	return compute(value, 1);
+}
+
+function addBinaryString(first: string, second: string): string {
+	var compute: Function = (result, b1, b2): string => {
+		if (b1.length === 0 && b2.length === 0) {
+			console.log(result)
+			return result;
+		}
+
+		var n1: string = b1.charAt(b1.length - 1);
+		var n2: string = b2.charAt(b2.length - 1);
+		var next_1: string = b1.slice(0, b1.length - 1);
+		var next_2: string = b2.slice(0, b2.length - 1);
+		if (next_1.length && !next_2.length) {
+			next_2 = '0'
+		} else if(!next_1.length && next_2.length) {
+			next_1 = '0'
+		}
+		if (n1 === '1' && n2 === '1') {
+			//ŒJ‚èã‚°
+			next_1 = addBinaryOne(next_1);
+			return compute('0' + result, next_1, next_2);
+		} else if (n1 === '0' && n2 === '0') {
+			return compute('0' + result, next_1, next_2);
+		} else {
+			return compute('1' + result, next_1, next_2);
+		}
+	}
+
+	return compute('', first, second);
+}
+
+function addBinaryOne(src: string): string {
+	var compute: Function = (pos: number, b: string): string => {
+		switch (b.charAt(b.length - 1 - pos)) {
+			case '0':
+				return head(b, pos) + '1' + tail(b, pos);
+				break;
+			case '1':
+				return compute(pos + 1, head(b, pos) + '0' + tail(b, pos));
+				break;
+			default:
+				return '1' + b;
+				break;
+		}
+	}
+
+	var head: Function = (str: string, pos: number) => {
+		return str.slice(0, str.length - (pos + 1));
+	}
+
+	var tail: Function = (str: string, pos: number) => {
+		return str.slice(str.length - pos, str.length);
+	}
+
+	return compute(0, src);
 }
 
 $(() => {
--- ts/chapter_1_test.ts
+++ ts/chapter_1_test.ts
@@ -3,7 +3,6 @@
 /// <reference path="../vendor/dts/zepto/zepto.d.ts" />
 /// <reference path="../vendor/dts/jasmine/jasmine.d.ts" />
 /// <reference path="./chapter_1.ts" />
-
 describe("numberToBaseString", () => {
 	it("base 10, 9 get 9", () => {
 		expect(numberToBaseString(9, 10)).toBe('9');
@@ -53,5 +52,45 @@ describe("baseStringToValue", () => {
 
 	it("base 16, ff get 255", () => {
 		expect(baseStringToValue('ff', 16)).toBe(255);
+	});
+});
+
+describe("addBinaryString", () => {
+	it("0 + 1 = 1", () => {
+		expect(addBinaryString('0', '1')).toBe('1');
+	});
+
+	it("1 + 1 = 10", () => {
+		expect(addBinaryString('1', '1')).toBe('10');
+	});
+
+	it("10 + 1 = 11", () => {
+		expect(addBinaryString('10', '1')).toBe('11');
+	});
+
+	it("1 + 11 = 100", () => {
+		expect(addBinaryString('11', '1')).toBe('100');
+	});
+
+	it("10 + 10 = 100", () => {
+		expect(addBinaryString('10', '10')).toBe('100');
+	});
+});
+
+describe("addBinaryString", () => {
+	it("0 -> 1", () => {
+		expect(addBinaryOne('0')).toBe('1');
+	});
+
+	it("1 -> 10", () => {
+		expect(addBinaryOne('1')).toBe('10');
+	});
+
+	it("10 -> 11", () => {
+		expect(addBinaryOne('10')).toBe('11');
+	});
+
+	it("11 -> 100", () => {
+		expect(addBinaryOne('11')).toBe('100');
 	});
 });
